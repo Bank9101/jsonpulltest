dff7f7ee29817e1274f494d51fbdd99703cd935d
Bank9101 <Bankaz123@gmail.com>
2025-09-11 02:18:27 +0700

    fix

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,2 @@
-python3 -m pip install -r requirements.txt
-python3 manage.py collectstatic --noinput --clear
+python3.13 -m pip install --upgrade pip setuptools wheel
+python3.13 -m pip install audioop-lts==0.2.1
