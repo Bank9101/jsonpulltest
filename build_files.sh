53b26b4fa2688e060e204f0b8f56ce40c708723e
Bank9101 <Bankaz123@gmail.com>
2025-09-11 02:22:10 +0700

    fix4

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Install dependencies
-python3.12 -m pip install -r requirements.txt
+python3.13 -m pip install -r requirements.txt
 
 # Reset and create database tables
-python3.12 manage.py reset_db
+python3.13 manage.py reset_db
 
 # Collect static files
-python3.12 manage.py collectstatic --noinput --clear
+python3.13 manage.py collectstatic --noinput --clear
