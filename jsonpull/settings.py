b382cbe917d3112bf9c58d51cbd554213ff5e52a
Bank9101 <Bankaz123@gmail.com>
2025-09-11 02:57:40 +0700

    again

--- jsonpull/settings.py
+++ jsonpull/settings.py
@@ -123,7 +123,7 @@ STATIC_URL = '/static/'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [Path.joinpath(BASE_DIR, 'statics')]
-STATIC_ROOT = Path.joinpath(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = Path.joinpath(BASE_DIR, 'staticfiles_build', 'statics')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
